4097833af13abd6398afdd139a7f38c13465f337
Daniel Choi <danielchoi1115@gmail.com>
2022-02-23 17:14:24 +0900

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,10 +6,12 @@ package Lab1;
 public class Library {
 	
 	/* Add this function */
+//	I have made a change
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
 			System.out.println("Hello, World!");
+		
 	}
 		
 	public boolean someLibraryMethod() {
