2f8021063fd2523f786fb89ed6186888fd4ee68f
hoshikon870 <743626197@qq.com>
2022-03-24 15:24:11 +0800

    test for branch partB

--- src/main/java/comp3111/covid/MyApplication.java
+++ src/main/java/comp3111/covid/MyApplication.java
@@ -47,7 +47,7 @@ public class MyApplication extends Application {
    		VBox root = (VBox) loader.load();
    		Scene scene =  new Scene(root);
    		stage.setScene(scene);
-   		stage.setTitle("Testing for hoshikon870");
+   		stage.setTitle("bracnch partB");
    		stage.show();
 	}
 
