d14f6a4b59c7c25db85024f81e584dd9f8fdbe7b
CHOI, Seung Ryeol <schoiak@connect.ust.hk>
2022-03-30 19:00:52 +0900

    Fix - removed foreach loop and used for loop

--- src/main/java/comp3111/labs/lab6/main.java
+++ src/main/java/comp3111/labs/lab6/main.java
@@ -6,11 +6,24 @@ package comp3111.labs.lab6;
 public class main {
 	public static void main(String[] args) {
 		Animal animals[] = new Animal[10];
-		for (Animal a : animals)
-			a = new Animal();	
+//		original code
+//		for (Animal a : animals)
+//			a = new Animal();
+
+//		fixed code
+//		HOW: use 'for' instead of 'for each' 
+		for (int i = 0; i < animals.length; i++) {
+			animals[i] = new Animal();	
+		}
 		for (int iii = 0; iii < 10; iii++) {
 			int ii = 0;
+//			                   WHERE: The bug was here. When entering animals[iii] where iii = 0
 			for (; ii < 100 && animals[iii].isAlive() ; ii++) {
+
+//				WHY: Values in array cannot be modified using 'for each' loop in JAVA 
+//				Therefore, nothing was assigned in animals[iii]
+//				and thus calling isAlive() method on a null object raised an error
+				
 				System.out.print(animals[iii].getWeight() + " ");
 				animals[iii].eat();
 				if (ii % 3 == 0)
