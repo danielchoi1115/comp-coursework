7b438e800548208fc7dfef9b64325a0240af7e55
hoshikon870 <743626197@qq.com>
2022-05-02 21:42:15 +0800

    Functions for calculating total death cases

--- src/main/java/comp3111/covid/DataAnalysis.java
+++ src/main/java/comp3111/covid/DataAnalysis.java
@@ -1,7 +1,16 @@
 package comp3111.covid;
 
-import org.apache.commons.csv.*;
-import edu.duke.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import edu.duke.FileResource;
 
 
 /**
@@ -106,5 +115,112 @@ public class DataAnalysis {
 			
 			return oReport;
 	 }
+	 
+	 public static String getRateOfTotalDeaths(String date, List<String> allCountry) {
+			String oReport = "";	
+		  for(String continent:allCountry) {
+			    Double fullyVaccinated = 0D;
+				long totalNumRecord = 0;
+				Double population = 0D;
+			for (CSVRecord rec : getFileParser("COVID_Dataset_v1.0.csv")) {
+				if (!rec.get("iso_code").equals("")) {
+					String s0=rec.get("date");
+					String c1=rec.get("location");
+				    String cparam=continent.split("-")[1].trim();
+				if(date==null || s0.equals(date) && cparam.equals(c1.trim())) {	
+					String s1 = rec.get("total_deaths");
+					String s2 = rec.get("total_deaths_per_million");		
+					if (!s1.equals("")) {
+						fullyVaccinated = Double.parseDouble(s1);
+						fullyVaccinated=fullyVaccinated+fullyVaccinated;
+					}
+					if (!s2.equals("")) {
+						population = Double.parseDouble(s2);
+						population=population+population;
+					}
+					
+					totalNumRecord++;
+				}		
+				
+				}
+			}
+		
+			oReport += "----------------"+continent+"----------------------\n";
+			oReport += String.format("date (%s): %,d Records\n\n", date, totalNumRecord);
+			oReport += String.format("[continent (%s)]\n", continent);
+			oReport += "Number of total_deaths : "+fullyVaccinated.toString()+"\n";
+			oReport += "total_deaths_per_million: "+population.toString()+"\n";
+			//oReport += "----------------------------------------------------\n";
+			}
+		    return oReport;
+	 }
+	 
+	 
+	 public static Map<String,Double>  getRateOfChart(String sdate,String edate,List<String> allCountry) {
+			String format="yyyy-MM-dd";
+			 long lsdate=gettimeStemp(sdate,format); 
+			 long ledate=gettimeStemp(edate,format);
+			 Map<String,Double> map=new HashMap<String,Double>();		
+			for(String continent:allCountry) {
+				 Double population = 0D;
+				 Double fullyVaccinated = 0D;
+				 long totalNumRecord = 0;
+			for (CSVRecord rec : getFileParser("COVID_Dataset_v1.0.csv")) {
+				if (!rec.get("iso_code").equals("")) {
+					String s0=rec.get("date");
+					String c1=rec.get("location");
+				    String cparam=continent.split("-")[1].trim();
+				     //2/24/2020
+				    String[] arrayDate=s0.split("/");
+				    s0=arrayDate[2]+"-"+(Integer.parseInt(arrayDate[0])<10?"0"+arrayDate[0]:arrayDate[0])+"-"+(Integer.parseInt(arrayDate[1])<10?"0"+arrayDate[1]:arrayDate[1]);
+				    long ldate=gettimeStemp(s0,format);   
+				if(lsdate<=ldate && ldate<=ledate && cparam.equals(c1.trim())) {	
+					String s1 = rec.get("total_deaths");
+					String s2 = rec.get("total_deaths_per_million");		
+					if (!s1.equals("")) {
+						fullyVaccinated = Double.parseDouble(s1);
+					}
+					if (!s2.equals("")) {
+						population = Double.parseDouble(s2);
+						Double dd=map.get(cparam);
+						if(dd==null) {
+						map.put(cparam, population);
+						}else {
+							dd=dd+population;
+							map.put(cparam, dd);	
+						}
+					}
+					
+					totalNumRecord++;
+				}		
+				
+				}
+			  }
+			}
+			
+			return map;
+	 }
+	 
+	 
+	 public static long gettimeStemp(String time, String format) {
+		 
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			if (!"".equals(format)) {
+				simpleDateFormat = new SimpleDateFormat(format);
+			}
+	 
+			long timeStemp = 0;
+			try {
+	 
+				Date date = simpleDateFormat.parse(time);
+	 
+				timeStemp = date.getTime();
+	 
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			return timeStemp;
+		}
  
+
 }
